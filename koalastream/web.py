d5566908c6d0eaf1ebb284069739728c9bf33236
Benjamin Rumble <therumbler@gmail.com>
2020-05-21 15:14:23 -0400

    added KS_STREAM_KEY env var

--- koalastream/web.py
+++ koalastream/web.py
@@ -1,8 +1,12 @@
 """Koala Stream FastAPI web service"""
 import logging
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Response
 
 logger = logging.getLogger(__name__)
+
+KS_STREAM_KEY = os.environ["KS_STREAM_KEY"]
 
 
 def make_api():
@@ -16,6 +20,7 @@ def make_api():
 
     @api.get("/token")
     async def token(
+        response: Response,
         app: str,
         flashver: str,
         swfurl: str,
@@ -28,6 +33,13 @@ def make_api():
         type: str,
     ):
         # logger.info("kwargs: %s", kwargs)
-        return {"sucess": True}
+        if name == KS_STREAM_KEY:
+            logger.debug('valid stream key')
+            return {"sucess": True}
+
+        logger.error('invalid stream key %s', KS_STREAM_KEY)
+        response.status_code = 403
+        return {"success": False}
+
 
     return api
