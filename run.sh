879a07bf20a0872fff437493e5c71f90885545f5
Benjamin Rumble <therumbler@gmail.com>
2020-05-22 19:18:24 -0400

    uvicorn log-level debug

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "running nginx..."
 
 /usr/local/nginx/sbin/nginx -c /koalastream/conf/nginx.conf
 cd /app
-pipenv run uvicorn main:api --host 0.0.0.0
+pipenv run uvicorn main:api --host 0.0.0.0 --log-level debug
