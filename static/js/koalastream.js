dd4ac9c801a43ad59f5a0dadc8a0849631a6fc3d
Benjamin Rumble <therumbler@gmail.com>
2020-06-03 19:44:19 -0700

    better front-end error handling

--- static/js/koalastream.js
+++ static/js/koalastream.js
@@ -2,10 +2,24 @@
     console.log('init');
     var form = document.querySelector('#signup');
     var messages = document.querySelector('#messages');
-    console.log(form);
+    function handleError(respJson){
+        console.error(respJson);
+        if(respJson.hasOwnProperty('error')){
+            console.error(respJson.error);
+            messages.innerHTML = respJson.error;
+        }
+        if(respJson.hasOwnProperty('detail')){
+            console.error(respJson.detail);
+            messages.innerHTML = respJson.detail[0].msg;
+            var selector = '#' + respJson.detail[0].loc[respJson.detail[0].loc.length -1 ];
+            console.log('selector', selector);
+            document.querySelector(selector).classList.add('error');
+        }
+    }
+    function handleSuccess(respJson){
+    }
     async function doSignup(evt) {
         evt.preventDefault();
-        console.log('signup');
         const data = {
             password1: document.querySelector('#password1').value,
             password2: document.querySelector('#password2').value,
@@ -13,9 +27,10 @@
         }
         const resp = await fetch('./users/signup', { method: 'POST', body: JSON.stringify(data) });
         const respJson = await resp.json();
-        if(respJson.hasOwnProperty('error')){
-            console.error(respJson.error);
-            messages.innerHTML = respJson.error;
+        if(resp.ok){
+            handleSuccess(respJson);
+        } else {
+            handleError(respJson);
         }
         return false;
     }
