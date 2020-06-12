f4c50746bff6471a4f5f6fb81c4ab0b6d3493ad1
Benjamin Rumble <therumbler@gmail.com>
2020-06-12 18:53:48 -0400

    logout

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,29 @@
-(function(){
+(function () {
     var form = document.querySelector('#startstream');
-    
-    async function doStartStream(evt){
+    var token;
+    async function call(endpoint, data, method) {
+        if (method == undefined) {
+            method = 'POST';
+        }
+        var headers = {
+            'Authorization': 'Bearer ' + token
+        }
+        var resp = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: headers
+        });
+        const respJson = await resp.json();
+        console.log('respJson', respJson);
+        return respJson;
+    }
+
+    function logout() {
+        window.localStorage.clear();
+        init();
+    }
+
+    async function doStartStream(evt) {
         evt.preventDefault();
         var url = 'server';
         var YOUTUBE_STREAM_KEY = document.querySelector('#YOUTUBE_STREAM_KEY').value;
@@ -18,16 +40,22 @@
             'Authorization': 'Bearer ' + localStorage.getItem('token')
         }
         var resp = await fetch(url, {
-            method:'POST', 
+            method: 'POST',
             body: JSON.stringify(data),
             headers: headers
-        }); 
+        });
         const respJson = await resp.json();
         console.error('respJson', respJson);
         return false
     }
-    function init(){
+    function init() {
+        token = localStorage.getItem('token');
+        if (!token) {
+            var pathname = window.location.pathname;
+            window.location.replace(pathname);
+        }
         form.addEventListener('submit', doStartStream);
+        document.querySelector('#logout').addEventListener("click", logout);
     }
     init();
 })();
